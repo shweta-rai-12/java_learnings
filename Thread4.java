26b001926dd779b88a01d94ec1e9af0d3deb927b
Shweta.Rai <shweta.rai@everbridge.com>
2026-05-16 16:25:55 +0530

    Updated the structure and files

--- Thread4.java
+++
@@ -1,7 +0,0 @@
-package threadAndMultithreading;
-
-public class Thread4 extends Thread{
-    public void run(){
-        System.out.println("Hey Hi Cuties "+Thread.currentThread().getName());
-    }
-}
